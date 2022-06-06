bfb839220f88629fcd4ee16aa0d933f6c5bcea76
BohmChad-FS <crbohm@student.fullsail.edu>
2022-06-05 20:42:17 -0400

    Restful Assignment Commit

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+const productRouter = require('./routes/Products')
 
-app.get('/products/all', (req, res) => {
-    res.send('Products, 2, price, desc')
-})
-
-app.get('/products/:productId-:productSize-:productColor', (req, res) => {
-    res.send('Product, ' + req.params.productId + ', ' + req.params.productSize + ', ' +req.params.productColor)
-})
-
-app.get('/products/:productId', (req, res) => {
-    res.send('Product, ' + req.params.productId)
-})
+app.use("/products", productRouter)
 
 app.listen(3000)
