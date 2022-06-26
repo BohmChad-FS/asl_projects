59afbf231ce33c8f88e340e26cb91a522a9bf2f5
BohmChad-FS <crbohm@student.fullsail.edu>
2022-06-26 18:48:48 -0400

    storefront index and show, as well as css. Need to finish.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use("/public", express.static('public'))
 
 const fileUpload = require('express-fileupload')
 app.use(fileUpload())
@@ -10,15 +9,23 @@ app.use(fileUpload())
 const productRouter = require('./routes/Products')
 const variantsRouter = require('./routes/Variants')
 const imagesRouter = require('./routes/Images')
+const storefrontRouter = require('./routes/StoreFront')
+const { index } = require('./controllers/StoreFront')
+
 app.set('views', __dirname + '/templates')
 app.set('view engine', 'twig')
+app.use("/public", express.static('public'))
 
-app.get('/', (req, res) => {
-    res.render("views/home")
-})
+app.get('/', 
+    index
+    // (req, res) => {
+    //     res.render("views/home")
+    // }
+)
 
 app.use("/products", productRouter)
 app.use("/variants", variantsRouter)
 app.use("/images", imagesRouter)
+app.use("/storefront", storefrontRouter)
 
 app.listen(3000)
--- models/product.js
+++ models/product.js
@@ -16,7 +16,10 @@ module.exports = (sequelize, DataTypes) => {
   }
   Product.init({
     title: DataTypes.STRING,
-    price: DataTypes.INTEGER
+    price: DataTypes.INTEGER,
+    slug: DataTypes.STRING,
+    description: DataTypes.TEXT('long'),
+    is_published: DataTypes.BOOLEAN
   }, {
     sequelize,
     modelName: 'Product',
